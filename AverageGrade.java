b0e49a3fce3f31202d260b118ac12c1f5e1a7656
wonka6 <keskine96@gmail.com>
2021-11-27 19:15:29 +0300

    Changes for better explanation

--- AverageGrade.java
+++ AverageGrade.java
@@ -13,7 +13,7 @@ public class AverageGrade {    //class
         math = input.nextInt();
 
         System.out.printf("Physic grade: ");
-        physic= input.nextInt();
+        physic = input.nextInt();
 
         System.out.printf("Turkish grade: ");
         turkish = input.nextInt();
@@ -24,32 +24,32 @@ public class AverageGrade {    //class
         System.out.printf("Music grade: ");
         music = input.nextInt();
 
-        if (0 < math && math < 100){
+        if (0 < math && math < 100) {
             total += math;
             i++;
         }
-        if (0 < physic && physic < 100){
+        if (0 < physic && physic < 100) {
             total += physic;
             i++;
         }
-        if (0 < turkish && turkish < 100){
+        if (0 < turkish && turkish < 100) {
             total += turkish;
             i++;
         }
-        if (0 < chemistry && chemistry < 100){
+        if (0 < chemistry && chemistry < 100) {
             total += chemistry;
             i++;
         }
-        if (0 < music && music < 100){
+        if (0 < music && music < 100) {
             total += music;
             i++;
         }
 
         average = total / i;
 
-        if(average <= 55){
+        if (average <= 55) {
             System.out.println("Failed! See you next year!");
-        }else{
+        } else {
             System.out.println("Passed! Congrats!");
         }
 
