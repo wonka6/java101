75ad9d09209f236ea61d00d2920c6ddcea0dd05a
wonka6 <keskine96@gmail.com>
2021-12-08 19:56:50 +0300

    The Main method to show the results on the screen.

---
+++ Classes/SalaryCalculator/Main.java
@@ -0,0 +1,8 @@
+package java101.Classes.SalaryCalculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("kemal", 2000, 45, 1985);
+        e1.toString();
+    }
+}
