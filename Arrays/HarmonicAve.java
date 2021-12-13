af930d540bab0126331d4ab0cfdee8a59ef8d501
wonka6 <keskine96@gmail.com>
2021-12-13 21:49:48 +0300

    You can find arrays examples with writtten in java

---
+++ Arrays/HarmonicAve.java
@@ -0,0 +1,15 @@
+package java101.Arrays;
+
+public class HarmonicAve {
+
+    public static void main(String[] args) {
+        double[] arr = {1, 2, 3, 4, 5, 6};
+        double sum = 1;
+        for(int i = 0; i < arr.length; i++){
+            sum += (1.0 / arr[i]);
+        }
+        double harmonic = arr.length / sum;
+
+        System.out.print("Harmonic Average : " + harmonic);
+    }
+}
