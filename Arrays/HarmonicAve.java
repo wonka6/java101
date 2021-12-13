9c87fc1615f1230f2ab5320194074bd959e7f206
wonka6 <keskine96@gmail.com>
2021-12-13 21:56:06 +0300

    The program prints the harmonic average of given arrays.

--- Arrays/HarmonicAve.java
+++ Arrays/HarmonicAve.java
@@ -5,7 +5,7 @@ public class HarmonicAve {
     public static void main(String[] args) {
         double[] arr = {1, 2, 3, 4, 5, 6};
         double sum = 1;
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             sum += (1.0 / arr[i]);
         }
         double harmonic = arr.length / sum;
