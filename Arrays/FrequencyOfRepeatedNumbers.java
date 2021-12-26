1deb4f4787e753e3c8fc76a462b6b6234e8ea57b
Elif Keskin <keskine96@gmail.com>
2021-12-26 23:57:30 +0300

    Find frequeny of repeated numbers but I cannot finish yet.

--- Arrays/FrequencyOfRepeatedNumbers.java
+++ Arrays/FrequencyOfRepeatedNumbers.java
@@ -1,30 +1,33 @@
 package java101.Arrays;
 
 public class FrequencyOfRepeatedNumbers {
+
+    public static boolean IsUnique(int[] list, int value) {
+        for (int i : list) {
+            if (i == value)
+                return true;
+        }
+        return false;
+    }
+
+
     public static void main(String[] args) {
-        int[] list = {7, 7, 4, 3, 3, 3};
+        int[] list = {7, 7, 7, 7, 4, 3, 3, 3, 7, 3};
 
-        int count = 0;
-        int fr = 0;
-        for(int i = 0; i < list.length; i++){
-
-            for(int j = 0; j < list.length; j++){
-                if((i != j) && (list[i] == list[j])){
+        for (int i = 0; i < list.length; i++) {
+            int count = 1;
+            for (int j = i; j < list.length; j++) {
+                if ((i != j) && (list[i] == list[j])) {
                     count++;
-                    System.out.print(list[i] + "\t" + count);
-                    fr = count;
+                }
+            }
+            if (!IsUnique(list, i)) {
+                if (count >= 2) {
+                    System.out.println(list[i] + " sayisi " + count + " kez tekrar etmistir.");
                     break;
                 }
             }
-            System.out.println(fr);
         }
-
-
-
-
-
-
 
     }
 }
-
